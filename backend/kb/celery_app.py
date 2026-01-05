7b1ecbd71e8dfea9cfebd0e4d3166a02a897d979
SichenLin1107 <sclin25@stu.pku.edu.cn>
2026-01-05 21:27:52 +0800

    Feature/update datasql (#56)
    * update frontend user management
    
    * update deploy
    
    * fix: 修复前端构建bug
    
    * fix：修复保存文档失败bug
    
    * 修复kb连接redis失败的bug
    
    * fix：修复初始化数据格式错误
    
    ---------
    
    Co-authored-by: liuxin <2323658175@qq.com>

--- backend/kb/celery_app.py
+++ backend/kb/celery_app.py
@@ -3,5 +3,5 @@ from celery import Celery
 # 根据实际部署修改 broker / backend 地址
 celery = Celery(
     "ai_agent_platform",
-    broker="redis://localhost:6379/0",  # 例：Redis 作为消息中间件
+    broker="redis://redis:6379/0",  # 例：Redis 作为消息中间件
     include=['kb.tasks'])
